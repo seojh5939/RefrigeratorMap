328b862846b2f07b5ef301aadd54f98a46ca96ec
SeoJungHan <seojh5939@gmail.com>
2023-07-13 17:56:38 +0900

    [DESIGN] : 🎨 장보기페이지 달력추가

--- lib/view/shopping/shopping_page.dart
+++ lib/view/shopping/shopping_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:refrigerator_map/viewModel/shopping_viewmodel.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 /// 장보기 페이지
 class ShoppingPage extends StatelessWidget {
@@ -6,6 +9,39 @@ class ShoppingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    ShoppingViewModel viewModel = context.read<ShoppingViewModel>();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(
+                left: 30,
+              ),
+              child: Text(
+                "지출 달력",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            TableCalendar(
+                focusedDay: context.watch<ShoppingViewModel>().focusedDay,
+                firstDay: DateTime(2023, 1, 1),
+                lastDay: DateTime(9999, 12, 31),
+                selectedDayPredicate: (day) {
+                  return viewModel.isSameDay(day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  if (!viewModel.isSameDay(selectedDay)) {
+                    viewModel.changeday(focusedDay, selectedDay);
+                  }
+                }),
+          ],
+        ),
+      ),
+    );
   }
 }
