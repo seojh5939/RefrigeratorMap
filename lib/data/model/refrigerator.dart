50d6468b00c4d2ec34ab1dda9d08808cc1f2d253
SeoJungHan <seojh5939@gmail.com>
2023-07-13 11:51:28 +0900

    [DESIGN] : 🎨 홈화면 UI 구성

--- lib/data/model/refrigerator.dart
+++ lib/data/model/refrigerator.dart
@@ -1,1 +1,17 @@
-class Refrigerator {}
+/// 식재료 모델클레스
+class Refrigerator {
+  Refrigerator({
+    required this.name,
+    this.count = 1,
+    required this.regDate,
+    required this.expDate,
+    required this.location,
+    this.memo = "",
+  });
+  String name; // 식재료명
+  int count; // 수량
+  String regDate; // 등록일자
+  String expDate; // 유통기한
+  String location; // 위치(냉장실, 냉동실, 신선칸)
+  String memo; // 메모
+}
---
+++ lib/data/model/shopping.dart
@@ -0,0 +1,11 @@
+/// 장보기 목록 모델클레스
+class Shopping {
+  Shopping({
+    this.isCompleted = false,
+    required this.content,
+    this.amount = 0,
+  });
+  bool isCompleted; // 완료박스 체크여부
+  String content; // 장보기 목록에 들어가는 내용
+  int amount; // 사용한 금액
+}
