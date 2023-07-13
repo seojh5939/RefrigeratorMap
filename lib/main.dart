ffbc3d0d07bdc9ea7992c4bc2658d80a55f2ffd8
SeoJungHan <seojh5939@gmail.com>
2023-07-13 20:11:44 +0900

    [FEAT] : ⚙ context 전역접근 클레스 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
+import 'package:refrigerator_map/util/global_variable.dart';
 import 'package:refrigerator_map/view/home/home_page.dart';
 import 'package:refrigerator_map/viewModel/bottom_navi_viewmodel.dart';
 import 'package:refrigerator_map/viewModel/shopping_viewmodel.dart';
@@ -9,13 +11,15 @@ late SharedPreferences prefs;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   prefs = await SharedPreferences.getInstance();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => BottomNaviViewModel()),
-        ChangeNotifierProvider(create: (context) => ShoppingViewModel()),
-      ],
-      child: const MyApp(),
+  initializeDateFormatting().then(
+    (_) => runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => BottomNaviViewModel()),
+          ChangeNotifierProvider(create: (context) => ShoppingViewModel()),
+        ],
+        child: const MyApp(),
+      ),
     ),
   );
 }
@@ -26,6 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: GlobalAccessContext.navigatorState,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
